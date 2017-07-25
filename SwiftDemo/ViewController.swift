e917a999b4389c34a86281f4606982fa3078c586
Ryan Han <rhan@surfline.com>
2017-07-24 22:56:57 -0700

    initial commit

---
+++ SwiftDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftDemo
+//
+//  Created by Ryan Han on 7/24/17.
+//  Copyright © 2017 Surfline. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
