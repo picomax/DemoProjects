76a25f40e5876cd4f7e33d2243af54d6cea8051e
picomax <picomax@gmail.com>
2017-07-28 09:14:47 -0700

    Added the test function of Alamofire.

--- SwiftDemo/ViewController.swift
+++ SwiftDemo/ViewController.swift
@@ -7,12 +7,15 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.doTesting()
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,5 +24,24 @@ class ViewController: UIViewController {
     }
 
 
+    func doTesting() {
+        let host = "https://itunes.apple.com/search"
+        var params = "term=jack+johnson"
+        var targetUrl = host + "?" + params;
+        
+        Alamofire.request(targetUrl).responseJSON { response in
+            print("Request: \(String(describing: response.request))")   // original url request
+            print("Response: \(String(describing: response.response))") // http url response
+            print("Result: \(response.result)")                         // response serialization result
+            
+            if let json = response.result.value {
+                print("JSON: \(json)") // serialized json response
+            }
+            
+            if let data = response.data, let utf8Text = String(data: data, encoding: .utf8) {
+                print("Data: \(utf8Text)") // original server data as UTF8 string
+            }
+        }
+    }
 }
 
